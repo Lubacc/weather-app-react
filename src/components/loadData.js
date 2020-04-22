b9666229ea3f72b926c9eb6b8c9859d3ab38c253
Lubacc <lyubomir.vaklinov@gmail.com>
2020-04-22 16:31:31 +0300

    Need some styling but works

--- src/components/loadData.js
+++ src/components/loadData.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import useFetch from './useFetch';
 import DisplayData from './DisplayData';
+require('dotenv').config();
 
 const InputOperations = ({ city, toFetch, setStep, setError }) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperal&appid=233595a30770d5e15c54753dd927ae1f`;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperal&appid=${API_KEY}`;
   const { data } = useFetch(url, toFetch);
 
   if (data && data.length !== 0) {
